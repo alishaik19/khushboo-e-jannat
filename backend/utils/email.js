b79d0267139e4c316cedd01fc872b5ad959559bc
Ali Shaikh <alishaikh1990@gmail.com>
2026-06-18 18:14:23 +0530

    fix bug

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,20 +1,29 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL,
-    to,
-    subject,
-    html,
-  });
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("EMAIL SENT SUCCESS");
+  } catch (err) {
+    console.log("EMAIL ERROR:", err);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
