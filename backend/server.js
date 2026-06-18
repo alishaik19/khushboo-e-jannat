acdda9940f0645cfb1b5225e0b7b05b256feeed0
Ali Shaikh <alishaikh1990@gmail.com>
2026-06-18 12:11:52 +0530

    Fix port for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const path = require("path");
 dotenv.config();
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // DB CONNECT
 const connectDB = require("./config/db");
